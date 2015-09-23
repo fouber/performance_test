503d767c64d68ededcc733d150920b68a30f08e7
fouber <fouber.NET@gmail.com>
2015-09-23 13:23:40 +0800

    [UPD] update some files.

--- log.js
+++ log.js
@@ -2,18 +2,21 @@ var index = 0;
 var last, start = last = Date.now();
 var out = process.argv[2];
 var fs = require('fs');
-var fd = fs.openSync(out, 'a');
+var fd = '';
 
 function pretty(data){
-    var log = [ '\n\n------------------ [' ];
+    var log = [  ];
     for(var key in data){
         log.push(key + ': ' + data[key]);
     }
-    log.push('] ------------------\n\n');
+    log.push();
     return log.join(' ');
 }
 
 module.exports = function(res){
+    res.on('end', function(){
+        fs.writeFileSync(out, fd);
+    });
     res.on('data', function(d){
         var now = Date.now();
         var info = pretty({
@@ -22,9 +25,9 @@ module.exports = function(res){
             cost: (now - last) + 'ms',
             total: (now - start) + 'ms'
         });
+        fd += d.toString('utf8');
+        fd += '\n\n------------------ [' + info + '] ------------------\n\n';
         console.log(info);
-        fs.writeSync(fd, d.toString('utf8'));
-        fs.writeSync(fd, info);
         last = now;
     });
 };
